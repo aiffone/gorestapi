5bebc20af865c29037691e1c2b85adda04b72f19
Kayode <aiffone@gmail.com>
2024-07-22 07:16:32 +0000

    infra

--- main.tf
+++ main.tf
@@ -3,11 +3,21 @@ provider "google" {
   project = "playground-s-11-5f7d5dd3"
 }
 
+# Enable monitoring and logging services
+resource "google_project_service" "monitoring" {
+  project = "playground-s-11-5f7d5dd3"
+  service = "monitoring.googleapis.com"
+}
+
+resource "google_project_service" "logging" {
+  project = "playground-s-11-5f7d5dd3"
+  service = "logging.googleapis.com"
+}
+
 # Define a Google Kubernetes Engine (GKE) cluster
 resource "google_container_cluster" "primary" {
-  name     = "my-go-rest-api-cluster"
-  location = "us-central1-a"
-
+  name               = "my-go-rest-api-cluster"
+  location           = "us-central1-a"
   initial_node_count = 1
 
   node_config {
@@ -22,6 +32,8 @@ resource "google_container_cluster" "primary" {
 
   # Enable legacy authorization (optional, based on your requirements)
   enable_legacy_abac = false
+
+
 }
 
 # Define the node pool for the cluster
@@ -37,15 +49,4 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
-}
-
-# Enable monitoring and logging
-resource "google_project_service" "monitoring" {
-  project = "playground-s-11-5f7d5dd3"
-  service = "monitoring.googleapis.com"
-}
-
-resource "google_project_service" "logging" {
-  project = "playground-s-11-5f7d5dd3"
-  service = "logging.googleapis.com"
 }
