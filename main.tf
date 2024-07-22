a61da5496927c38902c1459e0a78506ed3698ecb
Kayode <aiffone@gmail.com>
2024-07-22 07:44:12 +0000

    infra

--- main.tf
+++ main.tf
@@ -1,39 +1,31 @@
 provider "google" {
+  project = "playground-s-11-5f7d5dd3"
   region  = "us-central1"
-  project = "playground-s-11-5f7d5dd3"
-}
-
-# Enable monitoring and logging services
-resource "google_project_service" "monitoring" {
-  project = "playground-s-11-5f7d5dd3"
-  service = "monitoring.googleapis.com"
-}
-
-resource "google_project_service" "logging" {
-  project = "playground-s-11-5f7d5dd3"
-  service = "logging.googleapis.com"
 }
 
 # Define a Google Kubernetes Engine (GKE) cluster
 resource "google_container_cluster" "primary" {
-  name               = "my-go-rest-api-cluster"
-  location           = "us-central1-a"
+  name     = "my-go-rest-api-cluster"
+  location = "us-central1-a"
+
   initial_node_count = 1
 
   node_config {
-    machine_type = "e2-micro"  # Smallest machine type
+    machine_type = "e2-micro"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 
-  # Optional: Define a master version (remove if you want to use the latest version)
-  # master_version = "1.22.7-gke.300"
-
-  # Enable legacy authorization (optional, based on your requirements)
   enable_legacy_abac = false
 
-
+  lifecycle {
+    # Prevent Terraform from attempting to destroy or recreate this resource
+    prevent_destroy = true
+    ignore_changes  = [
+      # List any attributes you want to ignore changes for
+    ]
+  }
 }
 
 # Define the node pool for the cluster
@@ -44,9 +36,20 @@ resource "google_container_node_pool" "primary_nodes" {
   initial_node_count = 1
 
   node_config {
-    machine_type = "e2-micro"  # Smallest machine type
+    machine_type = "e2-micro"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
+}
+
+# Enable monitoring and logging
+resource "google_project_service" "monitoring" {
+  project = "playground-s-11-5f7d5dd3"
+  service = "monitoring.googleapis.com"
+}
+
+resource "google_project_service" "logging" {
+  project = "playground-s-11-5f7d5dd3"
+  service = "logging.googleapis.com"
 }
